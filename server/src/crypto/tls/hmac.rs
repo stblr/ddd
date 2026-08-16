30686e922d308b4946f05ad87a2a49f48e9e7462
Pablo Stebler <pablo@stebler.xyz>
2026-08-16 18:34:55 +0200

    Switch tls::hmac to Orion

--- server/src/crypto/tls/hmac.rs
+++ server/src/crypto/tls/hmac.rs
@@ -1,12 +1,13 @@
-use graviola::hashing::hmac::Hmac;
-use graviola::hashing::{Sha256, Sha384};
-use rustls::crypto;
+use orion::hazardous::mac::hmac::sha256::{self, HmacSha256};
+use orion::hazardous::mac::hmac::sha384::{self, HmacSha384};
+use rustls::crypto::hmac::{Hmac, Key, Tag};
 
 pub struct Sha256Hmac;
 
-impl crypto::hmac::Hmac for Sha256Hmac {
-    fn with_key(&self, key: &[u8]) -> Box<dyn crypto::hmac::Key> {
-        Box::new(Sha256HmacKey(Hmac::<Sha256>::new(key)))
+impl Hmac for Sha256Hmac {
+    fn with_key(&self, key: &[u8]) -> Box<dyn Key> {
+        let key = sha256::SecretKey::from_slice(key).unwrap();
+        Box::new(Sha256HmacKey(HmacSha256::new(&key)))
     }
 
     fn hash_output_len(&self) -> usize {
@@ -14,17 +15,17 @@ impl crypto::hmac::Hmac for Sha256Hmac {
     }
 }
 
-struct Sha256HmacKey(Hmac<Sha256>);
+struct Sha256HmacKey(HmacSha256);
 
-impl crypto::hmac::Key for Sha256HmacKey {
-    fn sign_concat(&self, first: &[u8], middle: &[&[u8]], last: &[u8]) -> crypto::hmac::Tag {
+impl Key for Sha256HmacKey {
+    fn sign_concat(&self, first: &[u8], middle: &[&[u8]], last: &[u8]) -> Tag {
         let mut ctx = self.0.clone();
         ctx.update(first);
         for m in middle {
             ctx.update(m);
         }
         ctx.update(last);
-        crypto::hmac::Tag::new(ctx.finish().as_ref())
+        Tag::new(ctx.finalize().unwrap().unprotected_as_bytes())
     }
 
     fn tag_len(&self) -> usize {
@@ -34,9 +35,10 @@ impl crypto::hmac::Key for Sha256HmacKey {
 
 pub struct Sha384Hmac;
 
-impl crypto::hmac::Hmac for Sha384Hmac {
-    fn with_key(&self, key: &[u8]) -> Box<dyn crypto::hmac::Key> {
-        Box::new(Sha384HmacKey(Hmac::<Sha384>::new(key)))
+impl Hmac for Sha384Hmac {
+    fn with_key(&self, key: &[u8]) -> Box<dyn Key> {
+        let key = sha384::SecretKey::from_slice(key).unwrap();
+        Box::new(Sha384HmacKey(HmacSha384::new(&key)))
     }
 
     fn hash_output_len(&self) -> usize {
@@ -44,17 +46,17 @@ impl crypto::hmac::Hmac for Sha384Hmac {
     }
 }
 
-struct Sha384HmacKey(Hmac<Sha384>);
+struct Sha384HmacKey(HmacSha384);
 
-impl crypto::hmac::Key for Sha384HmacKey {
-    fn sign_concat(&self, first: &[u8], middle: &[&[u8]], last: &[u8]) -> crypto::hmac::Tag {
+impl Key for Sha384HmacKey {
+    fn sign_concat(&self, first: &[u8], middle: &[&[u8]], last: &[u8]) -> Tag {
         let mut ctx = self.0.clone();
         ctx.update(first);
         for m in middle {
             ctx.update(m);
         }
         ctx.update(last);
-        crypto::hmac::Tag::new(ctx.finish().as_ref())
+        Tag::new(ctx.finalize().unwrap().unprotected_as_bytes())
     }
 
     fn tag_len(&self) -> usize {
